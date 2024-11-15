4483cc361735a33ad7d7d81639c1aa598a2113ac
GiaKhanh-LVC <giakhanhcafetung@gmail.com>
2024-11-15 21:47:36 +0700

    changes in ServoControlModule, HexMotorModule

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/Activity.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.control;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.control.ServoControlModule;
+import org.firstinspires.ftc.teamcode.control.MotorControlModule;
+import org.firstinspires.ftc.teamcode.control.HexMotorModule;
+public class Activity {
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/HexMotorModule.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/HexMotorModule.java
@@ -1,32 +1,23 @@
 package org.firstinspires.ftc.teamcode.control;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
 public class HexMotorModule {
 
 
-    public class MotorControlModule extends LinearOpMode {
+    public class MotorControlModule{
         private DcMotor motor;
 
-        @Override
         public void runOpMode() {
 
             motor = hardwareMap.get(DcMotor.class, "motorHex");
-
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             motor.setDirection(DcMotor.Direction.FORWARD);
-
-            waitForStart();
-
-            while (opModeIsActive()) {
-                // Điều khiển động cơ chạy với công suất 50%
-                motor.setPower(0.5);
-
-                // Thêm logic hoặc điều kiện dừng nếu cần thiết
-                sleep(1000); // Chạy trong 1 giây
-
-                // Dừng motor
-                motor.setPower(0);
-                sleep(1000); // Nghỉ trong 1 giây
-            }
+        }
+        public void SlideUp(){
+            motor.setPower(1);
+        }
+        public void SlideDown(){
+            motor.setPower(-1);
         }
     }
 
